122f0b8cc3f5db9bfcd74fd8c5a00481eb8729a6
yerasiju <144224665+yerasiju@users.noreply.github.com>
2025-05-01 15:00:22 +0500

    ww

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ function App() {
     <Router>
       <Layout>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route path="/bbtravel/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/login" element={<Login />} />
